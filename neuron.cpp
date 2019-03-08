06015038b08f7643aefb4853133460a9084d3203
Jan Paesmans <jan.paesmans@gmail.com>
2019-03-08 16:57:02 +0100

    Use a class template to specify the activation function.

--- neuron.cpp
+++ neuron.cpp
@@ -3,9 +3,19 @@
 #include <cmath>
 #include <stdexcept>
 
-static double
-sigmoid(double x) {
+double
+Sigmoid::operator()(double x) {
     return (1.0 / (1.0 + exp(-x)));
+}
+
+static double
+mean_squared_loss(std::vector<double> &expected, std::vector<double> &predicted) {
+    double squared_error = 0.0;
+    for (size_t i = 0; i < expected.size() && i < predicted.size(); i++) {
+        squared_error += pow((expected[i] - predicted[i]), 2.0);
+    }
+
+    return (squared_error / expected.size());
 }
 
 static double
@@ -18,13 +28,36 @@ dot_product(std::vector<double> &inputs, std::vector<double> &weights, double bi
     return d_product;
 }
 
-Neuron::Neuron(std::vector<double> &w, double b) : weights(w), bias(b) {}
+template<typename T>
+Neuron<T>::Neuron(std::vector<double> &w, double b) :
+    weights(w), bias(b), activation(new T()) {}
 
-double
-Neuron::feedForward(std::vector<double> &inputs) {
+template<typename T>
+Neuron<T>::Neuron(const Neuron<T> &n) :
+    weights(n.weights), bias(n.bias), activation(new T()) {}
+
+template<typename T>
+Neuron<T> & Neuron<T>::operator=(const Neuron<T> &rhs) {
+    Neuron<T> tmp(rhs);
+    weights = rhs.weights;
+    bias = rhs.bias;
+    activation = new T();
+
+    return *this;
+}
+
+template<typename T>
+Neuron<T>::~Neuron() {
+    delete activation;
+}
+
+template<typename T> double
+Neuron<T>::feedForward(std::vector<double> &inputs) {
     if (inputs.size() != weights.size()) {
         throw std::range_error("Different sizes!");
     }
 
-    return sigmoid(dot_product(inputs, weights, bias));
+    return (*activation)(dot_product(inputs, weights, bias));
 }
+
+template class Neuron<Sigmoid>;
--- neuron.hpp
+++ neuron.hpp
@@ -3,13 +3,29 @@
 
 #include <vector>
 
+class IActivationFunction {
+    public:
+        virtual double operator()(double x) = 0;
+        virtual ~IActivationFunction() {}
+};
+
+class Sigmoid: public IActivationFunction {
+    public:
+        double operator()(double x);
+};
+
+template<typename T>
 class Neuron {
     public:
-        Neuron(std::vector<double> &weights, double bias);
+        explicit Neuron(std::vector<double> &weights, double bias);
+        Neuron<T>(const Neuron<T> &n);
+        Neuron<T>& operator=(const Neuron<T> &n);
+        ~Neuron();
         double feedForward(std::vector<double> &inputs);
     private:
         std::vector<double> weights;
         double bias;
+        IActivationFunction *activation;
 };
 
 #endif
